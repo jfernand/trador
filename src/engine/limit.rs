9c42223a52d53da8e30f3efce605c140e09f1bab
javier <jfernand@me.com>
2025-04-26 18:39:05 -0400

    Add order fulfillment logic for Limit and improve Order struct
    Introduced a `fill_order` method to handle matching market and limit orders, reducing sizes accordingly. Made `size` and `orders` fields `pub(crate)` to support fulfillment logic. Added unit tests to verify order matching and correct size reduction behavior.

--- src/engine/limit.rs
+++ src/engine/limit.rs
@@ -4,7 +4,7 @@ use crate::engine::price::Price;
 #[derive(Debug)]
 pub struct Limit {
     price: Price,
-    orders: Vec<Order>,
+    pub(crate) orders: Vec<Order>,
 }
 
 impl Limit {
@@ -17,5 +17,20 @@ impl Limit {
 
     pub(crate) fn add_order(&mut self, order: Order) {
         self.orders.push(order);
+    }
+
+    pub(crate) fn fill_order(&mut self, market_order: &mut Order) {
+        for limit_order in &mut self.orders {
+            if market_order.size >= limit_order.size {
+                market_order.size -= limit_order.size;
+                limit_order.size = 0.0;
+            } else {
+                limit_order.size -= market_order.size;
+                market_order.size = 0.0;
+            }
+            if market_order.is_filled() {
+                break;
+            }
+        }
     }
 }
--- src/engine/orders.rs
+++ src/engine/orders.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Debug, Formatter};
+
 #[derive(Debug, Clone, Copy)]
 pub enum BidOrAsk {
     Bid,
@@ -8,17 +9,58 @@ pub enum BidOrAsk {
 #[derive(Clone, Copy)]
 pub struct Order {
     pub(crate) bid_or_ask: BidOrAsk,
-    size: f64,
+    pub(crate) size: f64,
 }
 
 impl Order {
     pub(crate) fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
         Order { bid_or_ask, size }
     }
+    pub(crate) fn is_filled(&self) -> bool {
+        self.size == 0.0
+    }
 }
 
 impl Debug for Order {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}: {}", self.bid_or_ask, self.size)
+        write!(f, "{:?}: {:#.2}", self.bid_or_ask, self.size)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::engine::limit::Limit;
+    use crate::engine::price::Price;
+
+    #[test]
+    fn limit_order_filled() {
+        let price = Price::new(10000.0);
+        let mut limit = Limit::new(price);
+        let buy_limit_order = Order::new(BidOrAsk::Bid, 100.0);
+        limit.add_order(buy_limit_order);
+
+        let mut market_sell_order = Order::new(BidOrAsk::Ask, 100.0);
+        limit.fill_order(&mut market_sell_order);
+        println!("{market_sell_order:?}");
+
+        println!("{limit:?}");
+        assert!(market_sell_order.is_filled());
+    }
+
+    #[test]
+    fn limit_order_not_filled() {
+        let price = Price::new(10000.0);
+        let mut limit = Limit::new(price);
+        let buy_limit_order = Order::new(BidOrAsk::Bid, 100.0);
+        limit.add_order(buy_limit_order);
+
+        let mut market_sell_order = Order::new(BidOrAsk::Ask, 99.0);
+        limit.fill_order(&mut market_sell_order);
+        println!("{market_sell_order:?}");
+
+        println!("{limit:?}");
+        assert!(market_sell_order.is_filled());
+        assert_eq!(limit.orders.get(0).unwrap().size, 1.0);
     }
 }
